eda792cd657603a2b9b99184c8c05f887626ee92
JosafatV <josav09@gmail.com>
2019-08-29 19:43:46 -0600

    added basic server

---
+++ server/main.c
@@ -0,0 +1,35 @@
+// Huazhong University of Cience and Technology
+// School of Computer Science
+//
+// Pico HTTP Server in C
+//
+// gitHub username: laobubu
+
+
+#include "httpd.h"
+
+int main(int c, char** v)
+{
+    serve_forever("12913");
+    return 0;
+}
+
+void route()
+{
+    ROUTE_START()
+
+    ROUTE_GET("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Hello! You are using %s", request_header("User-Agent"));
+    }
+
+    ROUTE_POST("/")
+    {
+        printf("HTTP/1.1 200 OK\r\n\r\n");
+        printf("Wow, seems that you POSTed %d bytes. \r\n", payload_size);
+        printf("Fetch the data using `payload` variable.");
+    }
+  
+    ROUTE_END()
+}
